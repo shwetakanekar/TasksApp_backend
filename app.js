b68ad72ff836d4a303ce554acb559a4d6914d78a
Shweta Kanekar <kanekarshweta10@gmail.com>
2022-07-09 20:59:31 +0530

    Made connection to DB and created model for task table.

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./app/models');
 
 const app = express();
-
+console.log(process.env);
 var corsOptions = {
   origin: 'http://localhost:8081',
 };
+
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+db.sequelize.sync();
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(() => {
+    console.log('Unable to connect to the database:', error);
+  });
 
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Tasks API.' });
---
+++ app/models/task.model.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize, Sequelize) => {
+  const Task = sequelize.define('task', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    title: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+
+  return Task;
+};
