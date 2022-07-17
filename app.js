6c563f99f870dfe7643bf67c7a235b11f8d61602
Shweta Kanekar <kanekarshweta10@gmail.com>
2022-07-17 17:53:45 +0530

    Updated frontend base url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('./app/models');
 const app = express();
 
 var corsOptions = {
-  origin: 'http://localhost:8081',
+  origin: 'http://localhost:4200',
 };
 
 app.use(cors(corsOptions));
